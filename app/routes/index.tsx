fdda1a5dc172d26b8388c225e1da315f7a065971
Maksim Zaytsev <maksim@zaytsev.cz>
2023-08-14 11:44:19 +0200

    [zaytsev] - fix button

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -28,7 +28,7 @@ export default function Index() {
                     View Notes for {user.email}
                   </Link>
                 ) : (
-                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-1 sm:gap-5 sm:space-y-0">
                     {/* <Link
                       to="/join"
                       className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
