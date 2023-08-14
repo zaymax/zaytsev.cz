8e290ca86d9c95e001492396588e308bd1f3d83a
Maksim Zaytsev <maksim@zaytsev.cz>
2023-08-14 11:01:47 +0200

    [zaytsev.cz] - comment link to registration page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -29,12 +29,12 @@ export default function Index() {
                   </Link>
                 ) : (
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
-                    <Link
+                    {/* <Link
                       to="/join"
                       className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
                     >
                       Sign up
-                    </Link>
+                    </Link> */}
                     <Link
                       to="/login"
                       className="flex items-center justify-center rounded-md bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
